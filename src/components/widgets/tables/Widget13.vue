45a5305d25e5bfccfc3fc7f8dc80158b4c2388af
RachataS <64030161@kmitl.ac.th>
2023-10-24 16:49:49 +0700

    can show data in table but not correct from

--- src/components/widgets/tables/Widget13.vue
+++ src/components/widgets/tables/Widget13.vue
@@ -1,128 +1,125 @@
 <template>
-  <!--begin::Tables Widget 13-->
   <div :class="widgetClasses" class="card">
-    <!--begin::Header-->
     <div class="card-header border-0 pt-5">
       <h3 class="card-title align-items-start flex-column">
         <span class="card-label fw-bold fs-3 mb-1">พนักงานบริษัท</span>
         <span class="text-muted mt-1 fw-semobold fs-7">Over 500 orders</span>
       </h3>
       <div class="card-toolbar">
-        <!--begin::Menu-->
         <button type="button" class="btn btn-sm btn-icon btn-color-primary btn-active-light-primary"
           data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end" data-kt-menu-flip="top-end">
           <KTIcon icon-name="category" icon-class="fs-2" />
         </button>
         <Dropdown2></Dropdown2>
-        <!--end::Menu-->
       </div>
     </div>
-    <!--end::Header-->
-
-    <!--begin::Body-->
     <div class="card-body py-3">
-      <!-- Use v-if to wait for data before rendering the table -->
-      <div v-if="newdata">
-        <!--begin::Table container-->
+      <div v-if="parsedData">
         <div class="table-responsive">
-          <!--begin::Table-->
           <table class="table table-row-bordered table-row-gray-100 align-middle gs-0 gy-3">
-            <!--begin::Table head-->
             <thead>
               <tr class="fw-bold text-muted">
-                <!-- ... Table head ... -->
+                <th>Date</th>
+                <th>Card ID</th>
+                <th>Name</th>
+                <th>Time In</th>
+                <th>Time Out</th>
               </tr>
             </thead>
-            <!--end::Table head-->
-
-            <!--begin::Table body-->
             <tbody>
-              <template v-for="(item, serialnumber) in newdata.RFID.Member" :key="serialnumber">
-                <tr>
-                  <td>
-                    <a href="#" class="text-dark fw-bold text-hover-primary fs-6">{{ serialnumber }}</a>
-                  </td>
-                  <td>
-                    <a href="#" class="text-dark fw-bold text-hover-primary d-block mb-1 fs-6">{{ item.Name }}</a>
-                  </td>
-                  <td>
-                    <a v-if="newdata.RFID.Datetime['8-10-2023'][serialnumber]?.Checkin"
-                      class="text-dark fw-bold text-hover-primary d-block mb-1 fs-6">
-                      {{ newdata.RFID.Datetime['8-10-2023'][serialnumber].Checkin.Time }}
-                    </a>
-                    <span v-else>-</span>
-                  </td>
-                  <td>
-                    <a v-if="newdata.RFID.Datetime['8-10-2023'][serialnumber]?.Checkout"
-                      class="text-dark fw-bold text-hover-primary d-block mb-1 fs-6">
-                      {{ newdata.RFID.Datetime['8-10-2023'][serialnumber].Checkout.Time }}
-                    </a>
-                    <span v-else>-</span>
-                  </td>
-                </tr>
+              <template v-if="parsedData && parsedData.Datetime && parsedData.Member">
+                <template v-for="(item, serialnumber) in parsedData.Member" :key="serialnumber">
+                  <tr>
+                    <td>
+                      <a href="#" class="text-dark fw-bold text-hover-primary fs-6">{{ serialnumber }}</a>
+                    </td>
+                    <td>
+                      <a href="#" class="text-dark fw-bold text-hover-primary d-block mb-1 fs-6">{{ item.Name }}</a>
+                    </td>
+                    <td>
+                      <a v-if="parsedData.Datetime['8-10-2023'][serialnumber]?.Checkin"
+                        class="text-dark fw-bold text-hover-primary d-block mb-1 fs-6">
+                        {{ parsedData.Datetime['8-10-2023'][serialnumber].Checkin.Time }}
+                      </a>
+                      <span v-else>-</span>
+                    </td>
+                    <td>
+                      <a v-if="parsedData.Datetime['8-10-2023'][serialnumber]?.Checkout"
+                        class="text-dark fw-bold text-hover-primary d-block mb-1 fs-6">
+                        {{ parsedData.Datetime['8-10-2023'][serialnumber].Checkout.Time }}
+                      </a>
+                      <span v-else>-</span>
+                    </td>
+                  </tr>
+                </template>
               </template>
             </tbody>
-            <!--end::Table body-->
           </table>
-          <!--end::Table-->
         </div>
-        <!--end::Table container-->
       </div>
       <div v-else>
-        <!-- Show a loading message or spinner while waiting for data -->
         Loading data...
       </div>
     </div>
-    <!--end::Body-->
   </div>
-  <!--end::Tables Widget 13-->
 </template>
 <script lang="ts">
-import { ref } from "vue";
-import { getAssetPath } from "@/core/helpers/assets";
-import { defineComponent, onMounted } from "vue";
+import { defineComponent, ref, type PropType, onMounted } from "vue";
 import Dropdown2 from "@/components/dropdown/Dropdown2.vue";
+import { getAssetPath } from "@/core/helpers/assets";
 import { initializeApp } from "firebase/app";
 import { getDatabase, ref as dbRef, get } from "firebase/database";
+import { number } from "yup";
 
 export default defineComponent({
-  name: "kt-widget-12",
+  name: "kt-widget-13",
   components: {
     Dropdown2,
   },
   props: {
     widgetClasses: String,
+    newdata: String,
   },
-  setup() {
-    const data = ref(null); // Initialize data as a ref
-    let newdata;
-
-    const firebaseConfig = {
-      apiKey: "AIzaSyAzugDwrepHZdvszOSFyGRga3IEg1qV-3c",
-      authDomain: "rfid-database-8c0f2.firebaseapp.com",
-      databaseURL: "https://rfid-database-8c0f2-default-rtdb.asia-southeast1.firebasedatabase.app",
-      projectId: "rfid-database-8c0f2",
-      storageBucket: "rfid-database-8c0f2.appspot.com",
-      messagingSenderId: "509413991821",
-      appId: "1:509413991821:web:fef2ffd91ade42f62fc9c4",
-      measurementId: "G-LMGX06N1SW"
+  data() {
+    return {
+      fetchedData: null,
+      data: ''
     };
+  },
+  computed: {
+    parsedData() {
+      try {
+        return JSON.parse(this.data);
+      } catch (error) {
+        console.error("Error parsing data:", error);
+        return null;
+      }
+    },
+  },
+  methods: {
+    async fetchData() {
+      const firebaseConfig = {
+        apiKey: "AIzaSyAzugDwrepHZdvszOSFyGRga3IEg1qV-3c",
+        authDomain: "rfid-database-8c0f2.firebaseapp.com",
+        databaseURL: "https://rfid-database-8c0f2-default-rtdb.asia-southeast1.firebasedatabase.app",
+        projectId: "rfid-database-8c0f2",
+        storageBucket: "rfid-database-8c0f2.appspot.com",
+        messagingSenderId: "509413991821",
+        appId: "1:509413991821:web:fef2ffd91ade42f62fc9c4",
+        measurementId: "G-LMGX06N1SW"
+      };
 
-    // Initialize Firebase
-    const app = initializeApp(firebaseConfig);
+      // Initialize Firebase
+      const app = initializeApp(firebaseConfig);
 
-    // Access the Realtime Database
-    const db = getDatabase(app);
-    const dataRef = dbRef(db, "RFID"); // Adjust the path as needed
-
-    // Function to retrieve data
-    const fetchData = async () => {
+      // Access the Realtime Database
+      const db = getDatabase(app);
+      const dataRef = dbRef(db, "RFID"); // Adjust the path as needed
       try {
         const snapshot = await get(dataRef);
         if (snapshot.exists()) {
           // Data found
-          const fetchedData = snapshot.val();
-          data.value = fetchedData; // Update the ref with fetched data
+          this.fetchedData = snapshot.val(); // Store data in the component's data property
 
         } else {
           console.log("No data found.");
@@ -130,22 +127,22 @@ export default defineComponent({
       } catch (error) {
         console.error("Error reading data:", error);
       }
-    };
-
-    // Fetch data on component mount
-    onMounted(async () => {
-      await fetchData();
-      newdata = data.value;
-      console.log(newdata);
-    });
-
+    },
+  },
+  async mounted() {
+    console.log('13 = ' + this.newdata);
+    await this.fetchData();
+    this.data = JSON.stringify(this.fetchedData);
+    console.log("Q13 = " + this.data);
+  },
+  setup() {
     return {
       getAssetPath,
-      newdata, // Make data available in the template
     };
   },
 });
 </script>
+
 
 <style>
 .card {
--- src/components/widgets/tables/newFile.ts
+++ src/components/widgets/tables/newFile.ts
@@ -1,32 +1,20 @@
-<template>
-  <!---HTML-->
-  <div>
-    <div class="mt-15">
-      <Chart1 :height="400"></Chart1>
-    </div>
-    <div class="mt-5">
-      <Table13 :newdata="data" />
-    </div>
-  </div>
-</template>
-
-<script>
-import { defineComponent, ref, onMounted } from "vue";
+import { defineComponent } from "vue";
 import { initializeApp } from "firebase/app";
-import Chart1 from "@/components/widgets/charts/Widget1.vue"
-import Table13 from "@/components/widgets/tables/Widget13.vue"
 import { getDatabase, ref as dbRef, get } from "firebase/database";
 
 export default defineComponent({
-  name: "Dashboard",
+  name: "kt-widget-13",
   components: {
-    Chart1,
-    Table13
+    Dropdown2,
+  },
+  props: {
+    widgetClasses: String,
+    newdata: String,
   },
   data() {
     return {
       fetchedData: null,
-      data: ''
+      data: Object,
     };
   },
   methods: {
@@ -34,12 +22,13 @@ export default defineComponent({
       const firebaseConfig = {
         apiKey: "AIzaSyAzugDwrepHZdvszOSFyGRga3IEg1qV-3c",
         authDomain: "rfid-database-8c0f2.firebaseapp.com",
-        databaseURL: "https://rfid-database-8c0f2-default-rtdb.asia-southeast1.firebasedatabase.app",
+        databaseURL:
+          "https://rfid-database-8c0f2-default-rtdb.asia-southeast1.firebasedatabase.app",
         projectId: "rfid-database-8c0f2",
         storageBucket: "rfid-database-8c0f2.appspot.com",
         messagingSenderId: "509413991821",
         appId: "1:509413991821:web:fef2ffd91ade42f62fc9c4",
-        measurementId: "G-LMGX06N1SW"
+        measurementId: "G-LMGX06N1SW",
       };
 
       // Initialize Firebase
@@ -53,7 +42,6 @@ export default defineComponent({
         if (snapshot.exists()) {
           // Data found
           this.fetchedData = snapshot.val(); // Store data in the component's data property
-
         } else {
           console.log("No data found.");
         }
@@ -63,15 +51,14 @@ export default defineComponent({
     },
   },
   async mounted() {
-    // Call the fetchData method using "this" to refer to the component
+    console.log("13 = " + this.newdata);
     await this.fetchData();
-    this.data = JSON.stringify(this.fetchedData);
-    console.log("data = " + this.data);
+    this.data = this.fetchedData;
+    console.log("Q13 = " + this.data);
   },
   setup() {
     return {
-
+      getAssetPath,
     };
   },
 });
-</script>
