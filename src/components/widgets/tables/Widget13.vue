013fd18f03fd5bfeb982377fed217e86952e8254
RachataS <64030161@kmitl.ac.th>
2023-10-24 20:41:49 +0700

    show correct format

--- src/components/widgets/tables/Widget13.vue
+++ src/components/widgets/tables/Widget13.vue
@@ -1,22 +1,33 @@
 <template>
+  <!--begin::Tables Widget 13-->
   <div :class="widgetClasses" class="card">
+    <!--begin::Header-->
     <div class="card-header border-0 pt-5">
       <h3 class="card-title align-items-start flex-column">
         <span class="card-label fw-bold fs-3 mb-1">พนักงานบริษัท</span>
         <span class="text-muted mt-1 fw-semobold fs-7">Over 500 orders</span>
       </h3>
       <div class="card-toolbar">
+        <!--begin::Menu-->
         <button type="button" class="btn btn-sm btn-icon btn-color-primary btn-active-light-primary"
           data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end" data-kt-menu-flip="top-end">
           <KTIcon icon-name="category" icon-class="fs-2" />
         </button>
         <Dropdown2></Dropdown2>
+        <!--end::Menu-->
       </div>
     </div>
+    <!--end::Header-->
+
+    <!--begin::Body-->
     <div class="card-body py-3">
+      <!-- Use v-if to wait for data before rendering the table -->
       <div v-if="parsedData">
+        <!--begin::Table container-->
         <div class="table-responsive">
+          <!--begin::Table-->
           <table class="table table-row-bordered table-row-gray-100 align-middle gs-0 gy-3">
+            <!--begin::Table head-->
             <thead>
               <tr class="fw-bold text-muted">
                 <th>Date</th>
@@ -26,42 +37,52 @@
                 <th>Time Out</th>
               </tr>
             </thead>
+            <!--end::Table head-->
+
+            <!--begin::Table body-->
             <tbody>
               <template v-if="parsedData && parsedData.Datetime && parsedData.Member">
-                <template v-for="(item, serialnumber) in parsedData.Member" :key="serialnumber">
-                  <tr>
-                    <td>
-                      <a href="#" class="text-dark fw-bold text-hover-primary fs-6">{{ serialnumber }}</a>
-                    </td>
-                    <td>
-                      <a href="#" class="text-dark fw-bold text-hover-primary d-block mb-1 fs-6">{{ item.Name }}</a>
-                    </td>
-                    <td>
-                      <a v-if="parsedData.Datetime['8-10-2023'][serialnumber]?.Checkin"
-                        class="text-dark fw-bold text-hover-primary d-block mb-1 fs-6">
-                        {{ parsedData.Datetime['8-10-2023'][serialnumber].Checkin.Time }}
-                      </a>
-                      <span v-else>-</span>
-                    </td>
-                    <td>
-                      <a v-if="parsedData.Datetime['8-10-2023'][serialnumber]?.Checkout"
-                        class="text-dark fw-bold text-hover-primary d-block mb-1 fs-6">
-                        {{ parsedData.Datetime['8-10-2023'][serialnumber].Checkout.Time }}
-                      </a>
-                      <span v-else>-</span>
-                    </td>
-                  </tr>
+                <template v-for="(date, dateKey) in parsedData.Datetime" :key="dateKey">
+                  <template v-for="(cardData, cardID) in date" :key="cardID">
+                    <tr>
+                      <td>{{ dateKey }}</td>
+                      <td>{{ cardID }}</td>
+                      <td>
+                        <a href="#" class="text-dark fw-bold text-hover-primary d-block mb-1 fs-6">
+                          {{ parsedData.Member[cardID].Name }}
+                        </a>
+                      </td>
+                      <td>
+                        <a v-if="cardData.Checkin" class="text-dark fw-bold text-hover-primary d-block mb-1 fs-6">
+                          {{ cardData.Checkin.Time }}
+                        </a>
+                        <span v-else>-</span>
+                      </td>
+                      <td>
+                        <a v-if="cardData.Checkout" class="text-dark fw-bold text-hover-primary d-block mb-1 fs-6">
+                          {{ cardData.Checkout.Time }}
+                        </a>
+                        <span v-else>-</span>
+                      </td>
+                    </tr>
+                  </template>
                 </template>
               </template>
             </tbody>
+            <!--end::Table body-->
           </table>
+          <!--end::Table-->
         </div>
+        <!--end::Table container-->
       </div>
       <div v-else>
+        <!-- Show a loading message or spinner while waiting for data -->
         Loading data...
       </div>
     </div>
+    <!--end::Body-->
   </div>
+  <!--end::Tables Widget 13-->
 </template>
 <script lang="ts">
 import { defineComponent, ref, type PropType, onMounted } from "vue";
